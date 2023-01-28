0bf884c0cfbfa32b9684ab3d9966343c9aa46e32
Jordan Dukes <jordandukes@Jordans-MBP.askey.com>
2023-01-28 11:10:25 -0800

    Added index.html file with paths included in code

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+const generateHTML = require("./generateHTML");
+
+const Engineer = require("./Engineer.js");
+
+const Intern = require("./Intern.js");
+
+const Manager = require("./Manager.js");
+
+
+
